111f9dc1882154a54260dfaaf446bcd0b2bac941
ramanan <nnraam@hotmail.com>
2021-01-21 15:37:52 +0100

    CORRECTION

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "../src/";
+import Container from "./Container";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="head">
+        <h1> Winc lil' Liedjeslijst</h1>
+      </div>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
